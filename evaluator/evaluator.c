e7231aaf55a90d55bd4aa22037195e4fc720b307
ekm01 <ekm7p8j@gmail.com>
2023-08-20 02:55:37 +0200

    fix evaluator

--- evaluator/evaluator.c
+++ evaluator/evaluator.c
@@ -150,3 +150,43 @@ long int evaluate(SyntaxTree* st) {
     exit(1);
 }
 
+unsigned int hash(char* key) {
+    unsigned int hash = 0;
+    while (*key != '\0') {
+        hash = (hash * 33) + (*key);
+        key++;
+    }
+    return hash % MAX_HASHMAP_SIZE;
+}
+
+HashMap* init() {
+    HashMap* map = (HashMap*) malloc(sizeof(HashMap));
+    if (map == NULL) {
+        fprintf(stderr, "Memory cannot be allocated!\n");
+        exit(1);
+    }
+    return map;
+}
+
+void insert(HashMap* map, char* key, Func func) {
+    unsigned int index = hash(key);
+    HashEntry* entry = (HashEntry*) malloc(sizeof(HashEntry));
+    if (entry == NULL) {
+        fprintf(stderr, "Memory cannot be allocated!\n");
+        exit(1);
+    }
+    entry->key = key;
+    entry->value = func;
+    map->map[index] = entry;
+}
+
+Func get(HashMap* map, char* key) {
+    unsigned int index = hash(key);
+    HashEntry* entry = map->map[index];
+
+    if (entry == NULL) {
+        fprintf(stderr, "No such entry!\n");
+        exit(1);
+    }
+    return entry->value;
+}
